c75c270c6d5e46a4269c4919ee680fbb7937719d
martin4324 <reynandez1121@gmail.com>
2025-10-28 23:17:25 -0400

    Agregué un menú y un avance del primer nivel

---
+++ Assets/Scripts/MoverFondo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoverFondo : MonoBehaviour
+{
+    public RawImage img;
+    public float x;
+    public float y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        img.uvRect = new Rect(img.uvRect.position + new Vector2(x, y) * Time.deltaTime, img.uvRect.size);
+    }
+}
